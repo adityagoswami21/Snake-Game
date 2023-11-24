ecb98f279305e114c68721b288a212a5397c002e
Chillpill9797 <adityagoswami193@gmail.com>
2023-11-24 22:26:07 +0530

    adding high score

--- main.py
+++ main.py
@@ -25,18 +25,18 @@ while game_is_on:
     # detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
-        score.clear()
-        score.update_score()
         snake.extend()
-        score.new_score += 1
+        score.increase_score()
     # detect collision with wall
-    if snake.head.xcor()>280 or snake.head.xcor()<-290 or snake.head.ycor()>280 or snake.head.ycor()<-290:
-        game_is_on = False
-        score.game_over()
+    if snake.head.xcor()>290 or snake.head.xcor()<-290 or snake.head.ycor()>290 or snake.head.ycor()<-290:
+        # game_is_on = False
+        score.reset_score()
+        snake.reset()
     # detect collision with body
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            score.game_over()
+            # game_is_on = False
+            score.reset_score()
+            snake.reset()
 screen.exitonclick()
 
--- scoreboard.py
+++ scoreboard.py
@@ -8,14 +8,25 @@ class Score(Turtle):
         self.goto(0, 270)
         self.color("white")
         self.new_score = 0
+        self.high_score = 0
         self.update_score()
 
     def update_score(self):
-        self.write(f"Score :{self.new_score} ", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score :{self.new_score}    High Score : {self.high_score} ", align=ALIGNMENT, font=FONT)
         self.hideturtle()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset_score(self):
+        if self.new_score > self.high_score:
+            self.high_score = self.new_score
+        self.new_score = 0
+        self.update_score()
 
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.new_score += 1
+        self.update_score()
 
