d6a0d6b2875c889b7170067e6eb65b9ff38eb13c
Chillpill9797 <adityagoswami193@gmail.com>
2023-11-25 22:45:36 +0530

    keeping the record of the high score everytime

--- scoreboard.py
+++ scoreboard.py
@@ -1,6 +1,8 @@
 from turtle import Turtle
 ALIGNMENT = "center"
 FONT = ('Algerian', 20, 'normal')
+
+
 class Score(Turtle):
     def __init__(self):
         super().__init__()
@@ -8,7 +10,9 @@ class Score(Turtle):
         self.goto(0, 270)
         self.color("white")
         self.new_score = 0
-        self.high_score = 0
+        file = open("data.txt")
+        self.high_score = file.read()
+        file.close()
         self.update_score()
 
     def update_score(self):
@@ -17,8 +21,10 @@ class Score(Turtle):
         self.hideturtle()
 
     def reset_score(self):
-        if self.new_score > self.high_score:
-            self.high_score = self.new_score
+        if int(self.new_score) > int(self.high_score):
+            self.high_score = str(self.new_score)
+            with open("data.txt", "w") as data:
+                data.write(f"{self.high_score}")
         self.new_score = 0
         self.update_score()
 
